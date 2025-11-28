96440b716dc12a056b720b2dceb993aa80f6f25e
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2025-11-28 12:44:20 -0500

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'https://apisiaaw.uaw.edu.ec/api/v1',
-  HOST_URL: 'https://apisiaaw.uaw.edu.ec/',
-  APP_NAME:'Sistema de Gestión Académica - SIAAW',
-  APP_SHORT_NAME:'SIAAW',
+  API_URL: 'https://api.yec-v2.yavirac.edu.ec/api/v1',
+  HOST_URL: 'https://api.yec-v2.yavirac.edu.ec/',
+  APP_NAME:'Sistema de Gestión Académica - YEC',
+  APP_SHORT_NAME:'YEC',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   API_URL: 'http://localhost:3000/api/v1',
   HOST_URL: 'http://localhost:3000/',
-  APP_NAME:'Sistema de Gestión Académica - SIAAW',
-  APP_SHORT_NAME:'SIAAW',
+  APP_NAME:'Sistema de Gestión Académica - YEC',
+  APP_SHORT_NAME:'YEC',
 };
